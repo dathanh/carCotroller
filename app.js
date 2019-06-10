521b3b25a1df136d2c22194df0bd08bbc026caff
dathanh <datthanh304@gmail.com>
2019-06-10 01:34:43 +0000

    update source code

--- app.js
+++ app.js
@@ -54,39 +54,11 @@ app.use(bodyParser.urlencoded({
 }));
 
 
-/***************Mongodb configuratrion********************/
-var mongoose = require('mongoose');
-// var configDB = require('./config/database.js');
-//configuration ===============================================================
-// mongoose.connect(configDB.url); // connect to our database
-
-
-require('./config/passport')(passport); // pass passport for configuration
-
-//set up our express application
-app.use(morgan('dev')); // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-//app.use(bodyParser()); // get information from html forms
-
 //view engine setup
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'app/views'));
 app.set('view engine', 'ejs');
-//app.set('view engine', 'ejs'); // set up ejs for templating
 
-
-//required for passport
-//app.use(session({ secret: 'iloveyoudear...' })); // session secret
-
-app.use(session({
-  secret: 'I Love India...',
-  resave: true,
-  saveUninitialized: true
-}));
-
-app.use(passport.initialize());
-app.use(passport.session()); // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
 
 // routes ======================================================================
 require('./config/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -14,9 +14,6 @@ var chokidar = require('chokidar');
 exports.home = function(req, res) {
 
   res.render('index.ejs', {
-    error: req.flash("error"),
-    success: req.flash("success"),
-    session: req.session,
     title: 'Car Controll',
 
   });
